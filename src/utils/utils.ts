fc3767db0d461ff979500de7a04fd83b55c68542
13gucci <bibabibum0110@gmail.com>
2024-08-19 15:38:42 +0700

    🏏 implement check access_token

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,6 @@
+import hc from '@/constants/http-status-codes';
+import { validationMsg } from '@/constants/messages/validation-messages';
+import { ErrorMessageCode } from '@/schemas/errors.schema';
 import bcrypt from 'bcrypt';
 
 export const generateHashPassword = ({
@@ -22,4 +25,26 @@ export const comparePassword = (myPlaintextPassword: string, hash: string) => {
         .compare(myPlaintextPassword, hash)
         .then((result) => result)
         .catch(() => false);
+};
+
+export const validateBearerToken = (token: string) => {
+    const parts = token.split(' ');
+
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        throw new ErrorMessageCode({
+            code: hc.BAD_REQUEST,
+            message: validationMsg.INVALID_TOKEN
+        });
+    }
+
+    const tokenValue = parts[1];
+
+    if (!tokenValue || tokenValue.length === 0) {
+        throw new ErrorMessageCode({
+            code: hc.BAD_REQUEST,
+            message: validationMsg.INVALID_TOKEN
+        });
+    }
+
+    return tokenValue;
 };
